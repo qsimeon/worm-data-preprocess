a3dc8dba8a9b3e5cd72e0d0a02ab2902b6e2ffda
hectorastrom <hector.astrom@gmail.com>
2025-01-21 13:55:44 -0500

    + requirements file + updated installation instructions

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 from preprocess._main import process_data, PREPROCESS_CONFIG, json
 
 if __name__ == "__main__":
+    print("Beginning preprocessing...\n")
     print("Configuration:", json.dumps(PREPROCESS_CONFIG, indent=2), end="\n\n")
     process_data(PREPROCESS_CONFIG)
